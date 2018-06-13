1254dce64fa6f8d7d326fb9f2c74f01e91347289
Xuan Feng <u6234544@anu.edu.au>
2018-06-13 21:50:47 +1000

    Q1 Tour

--- src/comp1110/exam/Q1Tour.java
+++ src/comp1110/exam/Q1Tour.java
@@ -1,6 +1,9 @@
 package comp1110.exam;
 
-import static comp1110.exam.Q1Tour.Piece.white;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * COMP1110 Final Exam, Question 1ii
@@ -42,6 +45,7 @@ public class Q1Tour {
         populateBoard(placement);
     }
 
+
     /**
      * Take a placement string and populate the board
      * @param placement A string describing placement of black and white pieces
@@ -51,7 +55,7 @@ public class Q1Tour {
             String piece = placement.substring(i,i+3);
             int col = (piece.charAt(1)-'A');
             int row = (piece.charAt(2)-'1');
-            this.pieces[col][row] = (piece.charAt(0) == 'B') ? Piece.black : white;
+            this.pieces[col][row] = (piece.charAt(0) == 'B') ? Piece.black : Piece.white;
         }
     }
 
@@ -65,89 +69,120 @@ public class Q1Tour {
      * starting point (according to the rules above).
      */
     public int tourVictims(String start) {
+        // FIXME Question 1iii: complete this function
+        int count = 0;
         int col = (start.charAt(0)-'A');
         int row = (start.charAt(1)-'1');
-        int result = 0;
-        if ((col+2>=0&&col+2<=7)&&(row+1>=0&&row+1<=7)) {
-            if (pieces[col + 2][row + 1] == white) {
-                result += 1;
-            } else if (pieces[col + 2][row + 1] == null) {
-                char newCol = (char) (start.charAt(0) + 2);
-                char newRow = (char) (start.charAt(1) + 1);
-                String newStart = String.valueOf(newCol) + String.valueOf(newRow);
-                result += tourVictims(newStart);
+        int[] knight = new int[]{col,row};
+        ArrayList<int[]> white = new ArrayList<>();
+        ArrayList<int[]> black = new ArrayList<>();
+        for (int x = 0; x <= 7; x = x + 1) {
+            for (int y = 0 ; y <= 7; y = y + 1) {
+                if (this.pieces[x][y] == Piece.black) {
+                    int[] blackOne = new int[]{x,y};
+                    black.add(blackOne);
+                } else if (this.pieces[x][y] == Piece.white) {
+                    int[] whiteOne = new int[]{x,y};
+                    white.add(whiteOne);
+                }
             }
         }
-        if ((col + 1>=0&&col + 1<=7)&&(row + 2>=0&&row + 2<=7)) {
-            if (pieces[col + 1][row + 2] == white) {
-                result += 1;
-            } else if (pieces[col + 1][row + 2] == null) {
-                char newCol = (char) (start.charAt(0) + 1);
-                char newRow = (char) (start.charAt(1) + 2);
-                String newStart = String.valueOf(newCol) + String.valueOf(newRow);
-                result += tourVictims(newStart);
+        ArrayList<int[]> togo = new ArrayList<>();
+        togo.add(knight);
+        int[] horizontal = new int[] {-1,1,2,2,1,-1,-2,-2};
+        int[] vertical = new int[] {-2,-2,-1,1,2,2,1,-1};
+        ArrayList<int[]> alreadygot = new ArrayList<>();
+        ArrayList<int[]> alreadyGone = new ArrayList<>();
+        while (togo.size() > 0) {
+            if (count == white.size()) break;
+            for (int i = 0; i < horizontal.length; i++) {
+                int newx = togo.get(0)[0] + horizontal[i];
+                int newy = togo.get(0)[1] + vertical[i];
+                if (newx >= 0 && newx <= 7 && newy <= 7 && newy >= 0
+                        && this.pieces[newx][newy] != Piece.black) {
+                    int[] ywu = {newx, newy};
+                    if (contains(white,ywu)) {
+                        if (!contains(alreadygot,ywu)) {
+                            count = count + 1;
+                            alreadygot.add(ywu);
+                            if (!contains(alreadyGone,ywu)) {
+                                togo.add(ywu);
+                            }
+                        }
+                    }
+                    else if (!contains(togo,ywu)) {
+                        if (!contains(alreadyGone,ywu))
+                            togo.add(ywu);
+                    }
+                }
             }
+            alreadyGone.add(togo.get(0));
+            togo.remove(0);
         }
-        if ((col+2>=0&&col+2<=7)&&(row - 1>=0&&row - 1<=7)) {
-            if (pieces[col + 2][row - 1] == white) {
-                result += 1;
-            } else if (pieces[col + 2][row - 1] == null) {
-                char newCol = (char) (start.charAt(0) + 2);
-                char newRow = (char) (start.charAt(1) - 1);
-                String newStart = String.valueOf(newCol) + String.valueOf(newRow);
-                result += tourVictims(newStart);
-            }
-        }
-        if ((col + 1>=0&&col + 1<=7)&&(row - 2>=0&&row - 2<=7)) {
-            if (pieces[col + 1][row - 2] == white) {
-                result += 1;
-            } else if (pieces[col + 1][row - 2] == null) {
-                char newCol = (char) (start.charAt(0) + 1);
-                char newRow = (char) (start.charAt(1) - 2);
-                String newStart = String.valueOf(newCol) + String.valueOf(newRow);
-                result += tourVictims(newStart);
-            }
-        }
-        if ((col - 2>=0&&col - 2<=7)&&(row+1>=0&&row+1<=7)) {
-            if (pieces[col - 2][row + 1] == white) {
-                result += 1;
-            } else if (pieces[col - 2][row + 1] == null) {
-                char newCol = (char) (start.charAt(0) - 2);
-                char newRow = (char) (start.charAt(1) + 1);
-                String newStart = String.valueOf(newCol) + String.valueOf(newRow);
-                result += tourVictims(newStart);
-            }
-        }
-        if ((col - 1>=0&&col - 1<=7)&&(row + 2>=0&&row + 2<=7)) {
-            if (pieces[col - 1][row + 2] == white) {
-                result += 1;
-            } else if (pieces[col - 1][row + 2] == null) {
-                char newCol = (char) (start.charAt(0) - 1);
-                char newRow = (char) (start.charAt(1) + 2);
-                String newStart = String.valueOf(newCol) + String.valueOf(newRow);
-                result += tourVictims(newStart);
-            }
-        }
-        if ((col-2>=0&&col-2<=7)&&(row-1>=0&&row-1<=7)) {
-            if (pieces[col - 2][row - 1] == white) {
-                result += 1;
-            } else if (pieces[col - 2][row - 1] == null) {
-                char newCol = (char) (start.charAt(0) - 2);
-                char newRow = (char) (start.charAt(1) - 1);
-                String newStart = String.valueOf(newCol) + String.valueOf(newRow);
-                result += tourVictims(newStart);
-            }
-        }
-        if ((col - 1>=0&&col - 1<=7)&&(row - 2>=0&&row - 2<=7)) {
-            if (pieces[col - 1][row - 2] == white) {
-                result += 1;
-            } else if (pieces[col - 1][row - 2] == null) {
-                char newCol = (char) (start.charAt(0) - 1);
-                char newRow = (char) (start.charAt(1) - 2);
-                String newStart = String.valueOf(newCol) + String.valueOf(newRow);
-                result += tourVictims(newStart);
-            }
-        }
-        return result;
+        return count;
     }
+    public boolean canReach (int[] poi) {
+        int[] horizontal = new int[] {-1,1,2,2,1,-1,-2,-2};
+        int[] vertical = new int[] {-2,-2,-1,1,2,2,1,-1};
+        ArrayList<int[]> theReach = new ArrayList<>();
+        for (int i = 0; i < horizontal.length ; i++) {
+            int newx = poi[0] + horizontal[i];
+            int newy = poi[1] + vertical[i];
+            if (newx >= 0 && newx <= 7 && newy <= 7
+                    && newy >= 0 && this.pieces[newx][newy] != Piece.black) {
+                int[] ywu = {newx,newy};
+                theReach.add(ywu);
+            }
+        }
+        if (theReach.size() > 0) {
+            return true;
+        }
+        return false;
+    }
+    public boolean contains (ArrayList<int[]> a,int[] b) {
+        for (int[] element:a) {
+            if (element[0] == b[0] && element[1] == b[1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
+
+//    private ArrayList<int[]> theReacha = new ArrayList<>();
+//    private boolean already = false;
+//    private int forstack = 0;
+//    public void allReachable (int[] poi, int[] knight) {
+//        this.theReacha.clear();
+//        this.already = false;
+//        int[] horizontal = new int[] {-1,1,2,2,1,-1,-2,-2};
+//        int[] vertical = new int[] {-2,-2,-1,1,2,2,1,-1};
+//        ArrayList<int[]> theReach = new ArrayList<>();
+//        for (int i = 0; i < horizontal.length ; i++) {
+//            int newx = poi[0] + horizontal[i];
+//            int newy = poi[1] + vertical[i];
+//            if (newx >= 0 && newx <= 7 && newy <= 7
+//                    && newy >= 0 && this.pieces[newx][newy] != Piece.black) {
+//                int[] ywu = {newx,newy};
+//                if (ywu == knight) {
+//                    this.already = true;
+//                    return;
+//                }
+//                if (!theReacha.contains(ywu)) {
+//                    this.theReacha.add(ywu);
+//                    theReach.add(ywu);
+//                }
+//            }
+//        }
+//
+//        if (theReach.size() != 0) {
+//            if (this.forstack < 3) {
+//                for (int[] element : theReach) {
+//                    allReachable (element,knight);
+//                }
+//                this.forstack += 1;
+//            }
+//        }
+//    }
+//}
